07d931583e76447755162a4cb87b1ad87e4c3f13
Aamir <119729846+Amirk692@users.noreply.github.com>
2023-02-23 03:21:37 +0530

    Update STRING_FUNCTION.sql

--- STRING_FUNCTION.sql
+++ STRING_FUNCTION.sql
@@ -227,6 +227,72 @@ select 'Bangalore, '|| NULL as null_example;
 select concat('Bangalore ', NVL(NULL,'')) as null_example;
 select 'Bangalore'|| NVL(NULL, '') as null_example;
 
+------------------------------------------------MYSQL---------------------------------------------------------------------
+
+USE DEMO_DATABASE;
+
+CREATE TABLE AGENTS
+   (	
+     AGENT_CODE CHAR(6) NOT NULL PRIMARY KEY, 
+	 AGENT_NAME CHAR(40) , 
+	 WORKING_AREA CHAR(35), 
+	 COMMISSION DECIMAL(10,2) DEFAULT 0.05, 
+	 PHONE_NO CHAR(15), 
+	 COUNTRY VARCHAR(25) 
+	 );
+
+INSERT INTO AGENTS VALUES ('A007', 'Ramasundar', 'Bangalore',0.15,'077-25814763', '');
+INSERT INTO AGENTS(AGENT_CODE,AGENT_NAME,WORKING_AREA) 
+VALUES ('A110', 'Anand', 'Germany');
+
+
+INSERT INTO AGENTS VALUES ('A003', 'Alex ', 'London', '0.13', '075-12458969', '');
+INSERT INTO AGENTS VALUES ('A008', 'Alford', 'New York', '0.12', '044-25874365', '');
+INSERT INTO AGENTS VALUES ('A011', 'Ravi Kumar', 'Bangalore', '0.15', '077-45625874', '');
+INSERT INTO AGENTS VALUES ('A010', 'Santakumar', 'Chennai', '0.14', '007-22388644', '');
+INSERT INTO AGENTS VALUES ('A012', 'Lucida', 'San Jose', '0.12', '044-52981425', '');
+INSERT INTO AGENTS VALUES ('A005', 'Anderson', 'Brisban', '0.13', '045-21447739', '');
+INSERT INTO AGENTS VALUES ('A001', 'Subbarao', 'Bangalore', '0.14', '077-12346674', '');
+INSERT INTO AGENTS VALUES ('A002', 'Mukesh', 'Mumbai', '0.11', '029-12358964', '');
+INSERT INTO AGENTS VALUES ('A006', 'McDen', 'London', '0.15', '078-22255588', '');
+INSERT INTO AGENTS VALUES ('A004', 'Ivan', 'Torento', '0.15', '008-22544166', '');
+INSERT INTO AGENTS VALUES ('A009', 'Benjamin', 'Hampshair', '0.11', '008-22536178', '');
+
+SELECT * FROM AGENTS;
+
+/* The SUBSTRING () function returns the position of a string or binary value from the complete string, 
+starting with the character specified by substring_start_index. If any input is null, null is returned */
+
+/*Example 1: Get the substring from a specific string in Snowflake*/
+
+SELECT SUBSTRING('MD AMIR', 1,6) AS PARTIAL_NAME;
+SELECT SUBSTRING(' MD AMIR', 1,6) AS PARTIAL_NAME;
+SELECT SUBSTRING('MD    AMIR', 1,6) AS PARTIAL_NAME;
+SELECT SUBSTRING ('MD    AMIR', 1,6) AS PARTIAL_NAME;
+SELECT SUBSTRING ('MD AMIR', 0,6) AS PARTIAL_NAME;-- HERE 0 AND 1 BOTH WORKS SAME
+SELECT SUBSTRING (' MD AMIR', 0,6) AS PARTIAL_NAME;-- HERE, 0 STARTS WITH THE SPACE BEFORE 'M'
+SELECT SUBSTRING ('  MD AMIR', 0,6) AS PARTIAL_NAME;-- HERE, 0 AND 1 STARTS WITH THE SPACE BEFORE 'M'
+SELECT SUBSTRING(' ANAND KUMAR JHA', 1,3) AS PARTIAL_NAME;
+SELECT SUBSTRING('ANAND KUMAR JHA', 1,3) AS PARTIAL_NAME;
+SELECT SUBSTR('MD AMIR', 1,6) AS PARTIAL_NAME;
+SELECT SUBSTR('MD AMIR', 0,6) AS PARTIAL_NAME;
+SELECT SUBSTR('MD AMIR', 0) AS PARTIAL_NAME;
+SELECT SUBSTR('MD AMIR', 1) AS PARTIAL_NAME;
+SELECT SUBSTR('MD AMIR', -4,4) AS PARTIAL_NAME;-- START FROM INDEX -7 AND GIVE NEXT 3 CHARACTERS FROM THAT INDEX
+SELECT SUBSTR('MD AMIR', -3) AS PARTIAL_NAME;
+
+-- Example 2: Get the substring from a specific string by using table data
+select AGENT_CODE,AGENT_NAME,substring(AGENT_CODE,-3,3) AS AGENT_INITIALS from agents;
+select AGENT_CODE,AGENT_NAME,substring(AGENT_CODE,2,5) AS AGENT_INITIALS from agents;
+
+-- AKASH
+-- sir if I want only A from anand and K FROM KUMAR SO IN THIS CASE WHAT SHOULD I DO
+
+select substring('Anand Kumar Jha',1,1) || substing('Anand Kumar Jha',7,1) as initial; -- THIS ONLY WORKS IN SNOWFLAKE(USING '||' FOR CONCAT)
+
+select concat(substring('ANAND KUMAR JHA',1,1),substring('ANAND KUMAR JHA',7,1),substring('ANAND KUMAR JHA',13,1)) AS partial_name;-- USE THIS INSTED
+
+
 
 
 
